5f708e08acf2bddf042e4a5fcddfcf23788d78f7
blazte <blazte.aster@gmail.com>
2023-08-21 08:49:08 -0500

    create h1 test name

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,8 +2,12 @@ import { expect, test } from '@playwright/test'
 
 test('should have the Country Quiz Game title page', async ({ page }) => {
     await page.goto('http://localhost:5173')
-    // const title = page.getByRole('heading', { name: 'utaheotuh' })
 
     await expect(page).toHaveTitle(/Country Quiz Game/)
-    // await expect(page)
+})
+
+test('should have h1 with country quiz name', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    const title = await page.$('h1')
+    expect(await title?.innerText()).toBe('COUNTRY QUIZ')
 })
