d20e3b0628b538f2c710913d6aae9c9f64c7b12f
blazte <blazte.aster@gmail.com>
2023-08-21 13:30:05 -0500

    show title of question after load countries

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,5 +1,7 @@
 import { expect, test } from '@playwright/test'
 
+import { configEnv } from '../src/utils/config-env'
+
 test('should have the Country Quiz Game title page', async ({ page }) => {
     await page.goto('http://localhost:5173')
 
@@ -10,4 +12,13 @@ test('should have h1 with country quiz name', async ({ page }) => {
     await page.goto('http://localhost:5173')
     const title = await page.$('h1')
     expect(await title?.innerText()).toBe('COUNTRY QUIZ')
+})
+
+test('should show options after fetch', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    await page.waitForLoadState('networkidle')
+    const subtitle = await page.$('h2')
+    console.log(await subtitle?.innerHTML())
+    expect(await subtitle?.innerText()).not.toBeNull()
+    expect(await subtitle?.innerText()).not.toBeUndefined()
 })
