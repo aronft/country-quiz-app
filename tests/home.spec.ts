a66910a51c7f73f16159fbc016f87f8325a96bf5
blazte <blazte.aster@gmail.com>
2023-08-22 15:49:49 -0500

    prove if the try again button works

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,16 +1,16 @@
 import { expect, test } from '@playwright/test'
 
-// test('should have the Country Quiz Game title page', async ({ page }) => {
-//     await page.goto('http://localhost:5173')
+test('should have the Country Quiz Game title page', async ({ page }) => {
+    await page.goto('http://localhost:5173')
 
-//     await expect(page).toHaveTitle(/Country Quiz Game/)
-// })
+    await expect(page).toHaveTitle(/Country Quiz Game/)
+})
 
-// test('should have h1 with country quiz name', async ({ page }) => {
-//     await page.goto('http://localhost:5173')
-//     const title = await page.$('h1')
-//     expect(await title?.innerText()).toBe('COUNTRY QUIZ')
-// })
+test('should have h1 with country quiz name', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    const title = await page.$('h1')
+    expect(await title?.innerText()).toBe('COUNTRY QUIZ')
+})
 
 test('workflow quiz game', async ({ page }) => {
     await page.goto('http://localhost:5173')
@@ -37,5 +37,7 @@ test('workflow quiz game', async ({ page }) => {
     expect(await resultsTitle?.textContent()).toBe('Results')
 
     const TryAgainButton = await page.getByRole('button', { name: 'Try again' })
+    expect(await TryAgainButton).toBeTruthy()
     await TryAgainButton.click()
+    expect(await buttons.count()).toBe(4)
 })
