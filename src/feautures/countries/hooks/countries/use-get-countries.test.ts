22c0f6e1b0e5044fd25de8a17c768955a5891aec
blazte <blazte.aster@gmail.com>
2023-08-23 04:59:25 -0500

    prove another way to mock file

--- src/feautures/countries/hooks/countries/use-get-countries.test.ts
+++ src/feautures/countries/hooks/countries/use-get-countries.test.ts
@@ -1,24 +1,29 @@
 import { getCountriesAPI } from '../../api/countries-api/get-countries-api'
 import { useGetCountries } from './use-get-countries'
 
-jest.mock('../../api/countries-api/get-countries-api')
+const mockedData = [
+    {
+        ccn3: '543',
+        name: {
+            common: 'Peru',
+        },
+        flags: {
+            png: 'https://',
+        },
+    },
+]
+jest.mock('../../api/countries-api/get-countries-api', () => {
+    return {
+        getCountriesAPI: jest.fn(() => mockedData),
+    }
+})
 describe('useGetCountries ', () => {
+    getCountriesAPI as jest.Mock
+
     test('should call getCountriesAPI function with the given data', async () => {
-        const mockedData = [
-            {
-                ccn3: '543',
-                name: {
-                    common: 'Peru',
-                },
-                flags: {
-                    png: 'https://',
-                },
-            },
-        ]
-        ;(getCountriesAPI as jest.Mock).mockReturnValue(mockedData)
         const { getCountries } = useGetCountries()
         const data = await getCountries()
-
         expect(data).toEqual(mockedData)
+        ;(getCountriesAPI as jest.Mock).mockReset()
     })
 })
